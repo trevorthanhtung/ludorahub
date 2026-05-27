c010fb9f58bdaf6f53728c9cbb6e5da0238959a3
trevorthanhtung <trevorthanhtung@github.com>
2026-05-27 23:57:09 +0700

    style(home): update player count to +5 for both games

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import GameCard from '../components/GameCard';
 import { useAppContext } from '../context/AppContext';
 
 const MOCK_GAMES = [
-  { id: 'true-or-dare', title: 'True or Dare', players: '2-20', time: '∞', tag: 'HOT', categories: ['party', 'small-group', 'pass-and-play'], image: '/tod-banner.webp', externalUrl: '/GAMES/TRUE OR DARE/index.html' },
-  { id: 'mafia', title: 'Ma Sói', players: '4-15', time: '30+', tag: 'SOON', comingSoon: true, categories: ['local-wifi'], image: '/ww-banner.webp' },
+  { id: 'true-or-dare', title: 'True or Dare', players: '+5', time: '∞', tag: 'HOT', categories: ['party', 'small-group', 'pass-and-play'], image: '/tod-banner.webp', externalUrl: '/GAMES/TRUE OR DARE/index.html' },
+  { id: 'mafia', title: 'Ma Sói', players: '+5', time: '30+', tag: 'SOON', comingSoon: true, categories: ['local-wifi'], image: '/ww-banner.webp' },
 ];
 
 const CATEGORY_TABS = [
